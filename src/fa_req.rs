4754a6b4afc6f1ce3e84af3a0143d6d9d6ca0d0b
iirelu <anna@bawk.space>
2019-07-25 08:44:14 +0200

    Not okay. Removed hardcoded login cookies.
    Don't worry, I logged out the last cookies.

--- src/fa_req.rs
+++ src/fa_req.rs
@@ -14,7 +14,7 @@ impl FaReq {
 
     pub fn get_submission_page(&self, id: u64) -> ReqResult<Response> {
         self.client.get(&format!("https://www.furaffinity.net/view/{}", id))
-            .header("Cookie", "b=fdeea4b8-6849-4cde-b18b-f716fd7334df; a=95b1300d-0e12-4314-8d6b-5c202b974caa;")
+            .header("Cookie", include_str!("LOGIN_COOKIE"))
             .send()
     }
 }
